52c30805a39d3dfbc43ce2f827027f42e0958a8e
Rangarju Viishhnu <123325064+Viishhnur@users.noreply.github.com>
2024-02-09 07:40:01 +0530

    Added deleteNode code

--- BSTOperations.cpp
+++ BSTOperations.cpp
@@ -1,145 +1,193 @@
 #include<iostream>
 #include<queue>
+#include "TreeNode.cpp"
 using namespace std;
-#include "Node.cpp"
 
-Node* insertToBST(int data ,Node* &root){
 
-    if(!root){
 
-        // Now insert here 
+Node*  insertToBST(Node* &root ,int data){
+    // Insertion in a BST is  of :- O(logn) Time complexity
+    if(root==NULL){
         root = new Node(data);
         return root;
-
     }
 
     if(data < root->val){
-        // Go left and insert 
-        root->left = insertToBST(data,root->left);
+        // Go to left
+        root->left = insertToBST(root->left,data);
+        
     }
 
     else{
-        // Go right and insert 
-        root->right = insertToBST(data,root->right);
+        // Go to left
+        root->right = insertToBST(root->right,data);
     }
 
     return root;
 
-
-
 }
 void takeInput(Node* &root){
     int data;
-    cout << "Enter data : " ;
-    cin >> data ;
+    cout << "\nEnter data : \n" ;
+    cin >> data;
+
     while(data!=-1){
-        root = insertToBST(data,root);
+        root = insertToBST(root,data);
         cin >> data;
     }
+
+    
 }
 
-void printBST(Node* root){
+bool searchInBST(Node* root,int val){
+    // Avg:- O(logn) worst case O(N) 
+    // Space :- O(H)
+    if(!root) return false;
 
-    if(!root) return;
-    queue<Node*>qu; 
-    qu.push(root);
+    if(val == root->val) return true;
 
-    while(!qu.empty()){
-        int queueSize = qu.size();
-
-        for(int i=0;i<queueSize;i++){
-            Node* Front = qu.front();
-            qu.pop();
-            if(Front->left) qu.push(Front->left);
-            if(Front->right) qu.push(Front->right);
-            cout << Front->val << " " ;
-        }
-        cout << endl;
-            
-       
+    if(val < root->val){
+        // Go and search in left
+        return searchInBST(root->left,val);
     }
-
-}
-
-bool searchInBST(Node* root,int data){
-
-    while(!root){
-
-        if(root->val == data) return true;
-
-        if(data < root->val){
-            // Go left and search
-            root = root->left;
-        }
-        else{
-            root = root->right;
-        }
-
-
+    if(val > root->val){
+        // Go and search in right
+        return searchInBST(root->right,val);
     }
 
     return false;
+
+
+
+    
+}
+void printBST(Node* root){
+
+
+    if(root==nullptr) return ;
+
+    // Level-Order means --> printing elements level wise 
+
+    queue<Node*> qu;
+    qu.push(root);
+
+    while(!qu.empty()){
+        
+        int queue_Size = qu.size();
+        for(int i=0;i<queue_Size;i++){
+            Node* Front = qu.front();
+            qu.pop();
+
+            if(Front->left) qu.push(Front->left);
+            if(Front->right) qu.push(Front->right);
+            cout << (Front->val) << " ";
+
+        }
+        cout << endl;
+
+    }
+
+    return ;
+
 }
 
-Node* Delete(Node *curr, int val)
-{
-    // The base case
-    if (curr == NULL)
-    {
-        return curr;
-    }
-    // Left Child
-    if (val < curr->val)
-    {
-        curr->left = Delete(curr->left, val);
-        return curr;
-    }
-    // Right Child
-    else if (val > curr->val)
-    {
-        curr->right = Delete(curr->right, val);
-        return curr;
+void printInOrder(Node* root){
+    if(!root) return;
+
+    printInOrder(root->left);
+    cout << root->val << " " ;
+    printInOrder(root->right);
+}
+
+int getMin(Node* root){
+    Node* temp = root;
+    while(temp->left!=NULL){
+        temp = temp->left;
     }
 
-    // Now, we've reached to the node which we want to delete.
-    // Our task is to check whether it has 0 or 1 or 2 children.
+    return temp->val;
+}
 
-    // If it has 0 or 1 child :
-
-    if (curr->left == NULL)
-    {
-        Node *temp = curr->right;
-        delete curr;
-        return temp;
+int getMax(Node* root){
+    Node* temp = root;
+    while(temp->right!=NULL){
+        temp = temp->right;
     }
-    else if (curr->right == NULL)
-    {
-        Node *temp = curr->left;
-        delete curr;
-        return temp;
+    return temp->val;
+}
+
+
+// int inOrderPredecessor(Node* root,Node* currNode){
+//     // get parent node of currNode
+//     // For that do inorder traversal , till u find current node
+//     Node* temp = root;
+
+//     while(temp!=currNode){
+        
+//     }
+
+// }
+
+
+Node* getLastRight(Node* root){
+    if(!root->right) return root;
+
+    return getLastRight(root->right);
+
+}
+Node* helper(Node* root){
+
+    // This helper makes useful in giving proper connections
+    if(!root->left) {
+        // Left child is not there , so return right subtree
+        return root->right;
+    }
+    else if(!root->right){
+        // Right subtree is not there , so return left subtree
+        return root->left;
     }
 
-    // Inorder Predecessor : Largest element in Left Subtree of the Current Node
-    // Inorder Successor   : Smallest element in Right Subtree of the current Node
-    // Or else if the node has 2 children :
-    // Find its Inorder Successor
-    // Copy the data of inorder successor and delete it
-    // Replace the curr node's data with Inorder successor's data (that you've copied earlier)
-    else
-    {
-        Node *tempParent = curr;
-        Node *temp = curr->right;
-        while (temp->left != NULL)
-        {
-            tempParent = temp;
-            temp = temp->left;
+    // If both child are present i.e both left child are right child are there
+    Node* rightChild = root->right;
+    Node* lastRight = getLastRight(root->left);
+    lastRight = rightChild;
+    return root->left;
+
+
+}
+Node* DeleteNode(Node* root,int key){
+    if(!root) return nullptr;
+    if(root->val == key) return helper(root);
+
+    Node* dummy = root;
+    while(dummy){
+        if(key < root->val){
+            // So the key may be present in left most part
+            if(root->val && root->left->val == key){
+                root->left = helper(root->left);
+                break;
+            }else{
+                root = root->left;
+            }
+
         }
-        int data = temp->val;
-        delete temp;
-        tempParent->left = NULL;
-        curr->val = data;
-        return curr;
+
+        else{
+
+            if(root->right && root->right->val){
+                root->right = helper(root->right);
+                break;
+            }else{
+                root = root->right;
+            }
+        }
+       
+
+
     }
+
+    return dummy;
+
 }
 int main(){
 
@@ -147,5 +195,23 @@ int main(){
     takeInput(root);
     printBST(root);
 
+    cout << "\nInorder traversal in BST comes in a sorted manner :\n" ;
+    printInOrder(root);
+    cout << endl;
+    
+    int N;
+    cout << "Enter number to be searched in BST : ";
+    cin >> N;
+    bool found = searchInBST(root,N);
+    if(found) cout << N << " Found\n";
+    else cout << N << " Not found\n";
+
+    cout << "\nMinimum value in given BST is : " << getMin(root);
+    cout << "\nMaximum value in given BST is : " << getMax(root) << endl;
+    root = DeleteNode(root,5);
+    printInOrder(root);
+
+
     return 0;
 }
+
